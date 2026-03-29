6a4a667c5b8fc221d8de0c385f0acf8eea1b2234
BatsCrow <twilliamsa776@gmail.com>
2026-03-28 22:06:34 -0400

    Recieve and predict using accel and gyro models

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,51 @@
 from flask import Flask
 from flask_sock import Sock
 import cv2
+import joblib
 import numpy as np
 import json
 from ultralytics import YOLO
+import pandas as pd
+
 
 app = Flask(__name__)
 sock = Sock(app)
 
 # Load your best.pt (ensure it's in the same folder)
 model = YOLO('best.pt') 
+
+accelModel = joblib.load('models/driving_accel_model.pkl')
+gyroModel = joblib.load('models/driving_gyro_model.pkl')
+
+data_store = {
+    "gyroMeasures": [],
+    "accelMeasures": []
+}
+
+
+SIGNAL_WINDOW = 100
+
+# Extract features for acceleration and gyroscope models
+def extract_features_from_json(data_list):
+    """
+    Converts the incoming WebSocket JSON list into the 8 features 
+    your model expects.
+    """
+    df = pd.DataFrame(data_list)
+    
+    # Vector Magnitudes
+    mag = np.sqrt(df['x']**2 + df['y']**2 + df['z']**2)
+    jerk = np.diff(mag)
+    
+    return [
+        mag.mean(),
+        mag.std(),
+        mag.max(),
+        mag.min(),
+        np.mean(np.abs(jerk)) if len(jerk) > 0 else 0,
+        mag.quantile(0.75) - mag.quantile(0.25)
+    ]
+
 
 @sock.route('/')
 def echo(ws):
@@ -21,9 +57,36 @@ def echo(ws):
                 # Handle JSON sensor data
                 try:
                     data = json.loads(message)
-                    # Detect if RN accidentally sent a Blob object instead of raw bytes
+
                     if isinstance(data, dict) and '_data' in data:
                         print("Warning: Received stringified Blob JSON! Ensure React Native sends ArrayBuffer.")
+                
+                    if data['type'] == 'gyro':
+                        data_store["gyroMeasures"].append(data['data'])
+                        if len(data_store["gyroMeasures"]) <= SIGNAL_WINDOW:
+                            features = extract_features_from_json(data_store["gyroMeasures"])
+                            prediction = int(gyroModel.predict([features])[0])
+
+                            if prediction != 0:
+                                ws.send(json.dumps({"type": "gyro_inference", "label": "Dangerous"}))
+                            else:
+                                ws.send(json.dumps({"type": "gyro_inference", "label": "Normal"}))
+
+                            data_store["gyroMeasures"] = []
+
+                    elif data['type'] == 'accelerometer':
+                        data_store["accelMeasures"].append(data['data'])
+                        if len(data_store["accelMeasures"]) <= SIGNAL_WINDOW:
+                            features = extract_features_from_json(data_store["accelMeasures"])
+                            prediction = int(accelModel.predict([features])[0])
+
+                            if prediction != 0:
+                                ws.send(json.dumps({"type": "accel_inference", "label": "Dangerous"}))
+                            else:
+                                ws.send(json.dumps({"type": "accel_inference", "label": "Normal"}))
+
+                            data_store["accelMeasures"] = []
+
                 except json.JSONDecodeError:
                     pass
                 continue
