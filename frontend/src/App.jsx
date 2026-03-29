5fb903bbd9db740e5eae2fed3d53022121cbbcae
bowen <botgroff@gmail.com>
2026-03-29 10:10:56 -0400

    dial color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { LineChart, Line, AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-const SERVER_URL = "ws://100.108.70.119:3000/frontend"; 
+const SERVER_URL = "ws://100.69.148.51:3000/frontend"; 
 const MAX_DATA_POINTS = 30;
 const MAX_LOGS = 5;
 
